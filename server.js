6d89678cadb40ede102d673eaf5e645e8fb1b915
Greyson Elkins <greysonelkins@gmail.com>
2020-09-01 16:22:27 -0600

    Change port listener

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 
 // Designate the port this server will run through
-app.set('port', process.env.port || 3001);
+app.listen(process.env.PORT || 5000)
 
 // Declare app-level middleware
 app.use(express.json());
