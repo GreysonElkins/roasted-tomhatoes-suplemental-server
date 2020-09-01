71d843d970bfc51d4608c32e48cd7825d97acab9
Greyson Elkins <greysonelkins@gmail.com>
2020-09-01 16:38:48 -0600

    Add settime out to Port listener

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 const cors = require('cors');
 
 // Designate the port this server will run through
-app.set(process.env.port || 5000);
+// app.set(process.env.port || 5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
 
 // Declare app-level middleware
 app.use(express.json());
